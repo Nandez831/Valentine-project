d1e2773fb16995f3c431437460f4e45c637d1fe8
Nandez831 <leoalt43@gmail.com>
2026-02-13 00:46:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,13 +35,13 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
+    "pleaseeeeeee",
+    "this is the Chud Button",
+    "sooo you're a chud?",
+    "I will EXPLODE ",
+    "PLEASE PLEASE PLEASE ",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Sike i lied SAY YES YOU CHUD"
 ];
 
 let messageIndex = 0;
